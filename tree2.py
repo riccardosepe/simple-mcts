56354610e1c1ac65ffec4b84ab0f596d4bfc6c78
riccardo <seric814@gmail.com>
2024-12-10 14:15:59 +0100

    Made the Node class private within the Tree class

--- tree2.py
+++ tree2.py
@@ -1,39 +1,63 @@
 """
 This file contains all the code relative to a tree data structure
 """
-
-class Node:
-    def __init__(self, parent_node, _id, max_children):
-        self._id = _id
-        self._children = []
-        self._parent_node = parent_node
-        self._max_children = max_children
-
-    def add_child(self, child):
-        self._children.append(child)
-
-    @property
-    def id(self):
-        return self._id
-
-    @property
-    def parent(self):
-        return self._parent_node
-
-    @property
-    def children(self):
-        return self._children
-
-    @property
-    def is_leaf(self):
-        return len(self._children) == 0
-
-    @property
-    def is_fully_expanded(self):
-        return len(self._children) == self._max_children
+import random
 
 
 class Tree:
+    class Node:
+        def __init__(self, parent_node, _id, legal_actions):
+            self._id = _id
+            self._children = dict.fromkeys(legal_actions, None)
+            self._parent_node = parent_node
+            self._available_moves = legal_actions[:]
+            self._visits = 0
+            self._score = 0
+
+        def add_child(self, child, move):
+            # NB: this method is only meant to be used within the Tree class
+            self._children[move] = child
+
+        def visit(self):
+            self._visits += 1
+
+        def increase_score(self, score):
+            self._score += score
+
+        def random_move(self, exclude=False):
+            move = random.choice(self._available_moves)
+            if exclude:
+                self._available_moves.remove(move)
+            return move
+
+        @property
+        def id(self):
+            return self._id
+
+        @property
+        def parent(self):
+            return self._parent_node
+
+        @property
+        def children(self):
+            return self._children
+
+        @property
+        def is_leaf(self):
+            return len(self._children) == 0
+
+        @property
+        def is_fully_expanded(self):
+            return len(self._available_moves) == 0
+
+        @property
+        def available_moves(self):
+            return self._available_moves
+
+        @property
+        def is_root(self):
+            return self._parent_node is None
+
     """
     NB: this tree is thought (for the moment) to support only environments with a maximum branching factor
     """
