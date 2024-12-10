1bf5fe1c703530c17036949eb242bd3368cc10e4
riccardo <seric814@gmail.com>
2024-12-10 14:14:37 +0100

    Filled the access point of the program

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
+import numpy as np
+
 from hanoi_env import TowersOfHanoiEnv
-from mcts import MCTS
+from mcts2 import MCTS
+from tictactoe_env import TicTacToeEnv
+
+HUMAN = True
+BOT = False
 
 
 def main():
-    env = TowersOfHanoiEnv()
+    env = TicTacToeEnv()
 
     obs = env.reset()
-    agent = MCTS(obs)
+    agent = MCTS(env)
 
+    player = True
+
+    while True:
+        obs = np.array(obs[0]).reshape((3,3))
+        print(obs)
+
+        if player:
+            action = int(input("Insert an action: "))
+            obs, _, done, _ = env.step(action)
+        else:
+            action = agent.plan(iterations_budget=10)
+            obs, _, done, _ = env.step(action)
+
+        if done:
+            break
+
+        player = not player
 
     
 if __name__ == '__main__':
