487ed2a9f15d2338008477689c90ca2cd8522dd0
riccardo <seric814@gmail.com>
2025-01-14 18:05:02 +0100

    Added some new abstract method to the class interface

--- base_env.py
+++ base_env.py
@@ -8,8 +8,25 @@ class BaseEnv(ABC, Env):
     def legal_actions(self):
         pass
 
+    @property
+    @abstractmethod
+    def _last_action(self):
+        pass
+
     @abstractmethod
     def backup(self):
+        # NB the data from the backup function can divide in two different categories
+        # 1. the data related to the actual state of the game
+        #     - board (remember to take a deepcopy or something)
+        #     - mark (whose turn it is)
+        #     - done
+        #     - last_action
+        # 2. the metadata used by MCTS
+        #     - reward
+        #     - player
+        #
+        # In particular, the first category can be whatever because it is handled internally by the environment, while
+        # the second category must be as it is because it's used outside the class
         pass
 
     @abstractmethod
@@ -18,4 +35,8 @@ class BaseEnv(ABC, Env):
 
     @abstractmethod
     def game_result(self):
+        pass
+
+    @abstractmethod
+    def reward(self):
         pass
