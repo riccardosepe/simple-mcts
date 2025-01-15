bbbdeb3faeca285f025e788d61381cf810d38931
riccardo <seric814@gmail.com>
2025-01-15 11:25:38 +0100

    Creating my version of frozen lake environment

---
+++ envs/frozenlake_env.py
@@ -0,0 +1,64 @@
+from gymnasium.envs.toy_text import FrozenLakeEnv
+
+from envs.base_env import BaseEnv
+
+
+class MyFrozenLakeEnv(BaseEnv, FrozenLakeEnv):
+    def __init__(self):
+        super().__init__()
+        self._last_reward = None
+        self.done = False
+
+    def reset(self, *args, **kwargs):
+        self._last_reward = None
+        self.done = False
+        return super().reset(*args, **kwargs)
+
+    @property
+    def legal_actions(self):
+        return list(range(4))
+
+    @property
+    def _last_action(self):
+        return super().lastaction
+
+    def step(self, a):
+        s, r, d, t, i = super().step(a)
+        self._last_reward = r
+        self.done = d
+        return s, r, d, t, i
+
+    @property
+    def adversarial(self):
+        return False
+
+    def backup(self):
+        checkpoint = {
+            'state': self.s,
+            'last_action': self._last_action,
+            'done': self.done,
+            'reward': self.reward(),
+            'player': 'Agent'
+        }
+
+    def load(self, checkpoint):
+        try:
+            super().s = checkpoint['state']
+            super().lastaction = checkpoint['last_action']
+            self._last_reward = checkpoint['reward']
+            self.done = checkpoint['done']
+        except KeyError:
+            return False
+        return True
+
+    def game_result(self):
+        if not self.done:
+            return "Game still running"
+        else:
+            if super().desc.flatten()[super().s] == b'G':
+                return "You made it!"
+            else:
+                return "You fell into an ice pit :("
+
+    def reward(self):
+        return self._last_reward
