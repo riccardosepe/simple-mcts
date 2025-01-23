43fabc7aef8a5c8e0d9a66555ce0d3358e49bfc8
riccardo <seric814@gmail.com>
2025-01-23 14:47:37 +0100

    Separate Node and Tree classes

--- src/tree.py
+++ src/tree.py
@@ -5,119 +5,120 @@ import random
 from functools import cmp_to_key
 
 
-class Tree:
-    class Node:
-        def __init__(self, parent_node, _id, legal_actions, game_data, action):
-            self._id = _id
-            self._children = dict.fromkeys(legal_actions, None)
-            self._parent_node = parent_node
-            self._available_actions = legal_actions[:]
-            self._visits = 0
-            self._score = 0
-            self._game_data = game_data
-            self._action = action
+class Node:
+    def __init__(self, parent_node, _id, legal_actions, game_data, action):
+        self._id = _id
+        self._children = dict.fromkeys(legal_actions, None)
+        self._parent_node = parent_node
+        self._available_actions = legal_actions[:]
+        self._visits = 0
+        self._score = 0
+        self._game_data = game_data
+        self._action = action
 
-        def __repr__(self):
-            return f"{self.player}(id={self._id}, visits={self._visits}, score={self._score}, action={self._action})"
+    def __repr__(self):
+        return f"{self.player}(id={self._id}, visits={self._visits}, score={self._score}, action={self._action})"
 
-        def add_child(self, child):
-            # NB: this method is only meant to be used within the Tree class
-            self._children[child.action] = child
-            self._available_actions.remove(child.action)
+    def add_child(self, child):
+        # NB: this method is only meant to be used within the Tree class
+        self._children[child.action] = child
+        self._available_actions.remove(child.action)
 
-        def visit(self):
-            self._visits += 1
+    def visit(self):
+        self._visits += 1
 
-        def update_score(self, score):
-            self._score += score
+    def update_score(self, score):
+        self._score += score
 
-        def random_action(self):
-            action = random.choice(self._available_actions)
-            return action
+    def random_action(self):
+        action = random.choice(self._available_actions)
+        return action
 
-        def set_root(self):
-            assert self._parent_node is not None
-            self._parent_node = None
+    def set_root(self):
+        assert self._parent_node is not None
+        self._parent_node = None
 
-        def ply(self, action):
-            assert self.is_root
-            self._available_actions.remove(action)
-            del self._children[action]
+    def ply(self, action):
+        assert self.is_root
+        self._available_actions.remove(action)
+        del self._children[action]
 
-        @property
-        def best_child(self):
-            children_list = list(self._children.values())
-            return sorted(children_list, key=cmp_to_key(Tree.Node.node_cmp))[0]
+    @property
+    def best_child(self):
+        children_list = list(self._children.values())
+        return sorted(children_list, key=cmp_to_key(Node.node_cmp))[0]
 
-        @staticmethod
-        def node_cmp(this, other):
-            if this.visits > other.visits:
+    @staticmethod
+    def node_cmp(this, other):
+        if this.visits > other.visits:
+            return -1
+        elif this.visits < other.visits:
+            return 1
+        else:
+            if this.score > other.score:
                 return -1
-            elif this.visits < other.visits:
+            elif this.score < other.score:
                 return 1
             else:
-                if this.score > other.score:
-                    return -1
-                elif this.score < other.score:
-                    return 1
-                else:
-                    # break ties randomly
-                    return random.choice([-1, 1])
+                # break ties randomly
+                return random.choice([-1, 1])
 
-        @property
-        def id(self):
-            return self._id
+    @property
+    def id(self):
+        return self._id
 
-        @property
-        def parent(self):
-            return self._parent_node
+    @property
+    def parent(self):
+        return self._parent_node
 
-        @property
-        def children(self):
-            return self._children
+    @property
+    def children(self):
+        return self._children
 
-        @property
-        def is_leaf(self):
-            return all(map(lambda x: x is None, self._children.values()))
+    @property
+    def is_leaf(self):
+        return all(map(lambda x: x is None, self._children.values()))
 
-        @property
-        def is_fully_expanded(self):
-            return len(self._available_actions) == 0
+    @property
+    def is_fully_expanded(self):
+        return len(self._available_actions) == 0
 
-        @property
-        def available_actions(self):
-            return self._available_actions
+    @property
+    def available_actions(self):
+        return self._available_actions
 
-        @property
-        def is_root(self):
-            return self._parent_node is None
+    @property
+    def is_root(self):
+        return self._parent_node is None
 
-        @property
-        def score(self):
-            return self._score
+    @property
+    def score(self):
+        return self._score
 
-        @property
-        def visits(self):
-            return self._visits
+    @property
+    def visits(self):
+        return self._visits
 
-        @property
-        def is_terminal(self):
-            return self._game_data['done']
+    @property
+    def is_terminal(self):
+        return self._game_data['done']
 
-        @property
-        def game_reward(self):
-            return self._game_data['reward']
+    @property
+    def game_reward(self):
+        return self._game_data['reward']
 
-        @property
-        def action(self):
-            return self._action
+    @property
+    def action(self):
+        return self._action
 
-        @property
-        def player(self):
-            return self._game_data['player']
+    @property
+    def player(self):
+        return self._game_data['player']
+
+class Tree:
 
     def __init__(self, root_legal_actions, root_data):
-        self._root = Tree.Node(None, 0, root_legal_actions, root_data, None)
+        self._root = Node(None, 0, root_legal_actions, root_data, None)
         self._nodes = {0: self._root}
         self._last_id = 0
 
@@ -132,7 +133,7 @@ class Tree:
         parent = self._nodes[parent_id]
         new_id = self._last_id + 1
         self._last_id = new_id
-        new_node = Tree.Node(parent, new_id, legal_actions, node_data, action)
+        new_node = Node(parent, new_id, legal_actions, node_data, action)
         parent.add_child(new_node)
         self._nodes[new_id] = new_node
         return new_node
