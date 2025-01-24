d3ce1915f0ae66314029767a2f7e2e0da633b625
riccardo <seric814@gmail.com>
2025-01-24 17:15:43 +0100

    Updated main, working with keep_subtree=True

--- main_chance.py
+++ main_chance.py
@@ -10,16 +10,16 @@ def main():
     env = MyFrozenLakeEnv(render_mode='human', is_slippery=True, map_name='4x4')
     env.reset(seed=SEED)
 
-    # agent = ChanceMCTS(env, seed=SEED, adversarial=env.adversarial, gamma=1, keep_subtree=False)
+    agent = ChanceMCTS(env, seed=SEED, adversarial=env.adversarial, gamma=1, keep_subtree=True, max_depth=50)
 
     env.render()
 
     done = False
     i = 0
     while not done:
-        agent = ChanceMCTS(env, seed=SEED, adversarial=env.adversarial, gamma=1, keep_subtree=False, max_depth=50)
         action = agent.plan(iterations_budget=10000)
         obs, _, done, _, _ = env.step(action)
+        agent.determinize_chance_node(obs)  # TODO: hash states
         i += 1
 
         env.render()
