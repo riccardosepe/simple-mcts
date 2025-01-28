26b727d022a65a57082c66f382fb30ec58ac3fe3
riccardo <seric814@gmail.com>
2025-01-28 12:15:00 +0100

    If keep_subtree is False, don't determinize the chance node

--- main_chance.py
+++ main_chance.py
@@ -12,8 +12,9 @@ def main():
     print("Using seed ", SEED)
     env = MyFrozenLakeEnv(render_mode='human', is_slippery=True, map_name='4x4')
     env.reset(seed=SEED)
+    keep_subtree = False
 
-    agent = ChanceMCTS(env, seed=SEED, adversarial=env.adversarial, gamma=1, keep_subtree=True, max_depth=50)
+    agent = ChanceMCTS(env, seed=SEED, adversarial=env.adversarial, gamma=1, keep_subtree=keep_subtree, max_depth=50)
 
     env.render()
 
@@ -22,10 +23,11 @@ def main():
     while not done:
         action = agent.plan(iterations_budget=10000)
         obs, _, done, _, _ = env.step(action)
-        agent.determinize_chance_node(obs)  # TODO: hash states
         i += 1
-
         env.render()
+
+        if keep_subtree:
+            agent.determinize_chance_node(obs)  # TODO: hash states
 
     print(env.game_result())
     env.close()
